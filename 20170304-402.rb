a26f972c3288d6eee38410be3349c41d7e7689a0
yuzixun <yzxun@qq.com>
2017-03-05 17:36:32 +0800

    优化0304中的算法

--- 20170304-402.rb
+++ 20170304-402.rb
@@ -8,15 +8,13 @@ def remove_kdigits(num, k)
 
   result = ''
   num.each_char do |element|
-    last_char = result.slice(-1)
-    while k > 0 && last_char && last_char > element
+    while k > 0 && result.slice(-1) && result.slice(-1) > element
       result.chop!
       k -= 1
-      last_char = result.slice(-1)
     end
     result.concat(element)
   end
 
-  result = result[0..result.size-1-k].sub!(/^0*/, '')
+  result = result[0..result.size-1-k].sub(/^0*/, '')
   result.empty? ? '0' : result
 end
