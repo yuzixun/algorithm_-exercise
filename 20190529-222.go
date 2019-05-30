daaa59201b6580d19ea53563363ed0671d75de4a
yuzixun <yzxun@qq.com>
2019-05-31 00:04:31 +0800

    0529 - 0530

---
+++ 20190529-222.go
@@ -0,0 +1,38 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func countNodes(root *TreeNode) int {
+	return counter(0, root)
+}
+
+func counter(count int, root *TreeNode) int {
+	if root == nil {
+		return count
+	}
+	if root.Left != nil {
+		count = counter(count, root.Left)
+	}
+	if root.Right != nil {
+		count = counter(count, root.Right)
+	}
+
+	return count + 1
+}
+
+// func main() {
+
+// }
---
+++ 20190530-763.go
@@ -0,0 +1,28 @@
+package main
+
+func partitionLabels(S string) []int {
+	result := []int{}
+	stringToPos := make(map[rune]int)
+
+	start, last := 0, 0
+	for i, l := range S {
+		stringToPos[l] = i
+	}
+	for i, l := range S {
+		if last < stringToPos[l] {
+			last = stringToPos[l]
+		}
+
+		if last == i {
+			result = append(result, last-start+1)
+			start = last + 1
+		}
+	}
+
+	// fmt.Println(result)
+	return result
+}
+
+func main() {
+	partitionLabels("ababcbacadefegdehijhklij")
+}
