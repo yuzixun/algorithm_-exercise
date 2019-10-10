a62c4a8b872ca0ca09b287436e0c45f2f7ace100
yuzixun <yzxun@qq.com>
2019-10-10 23:08:59 +0800

    42. Trapping Rain Water

---
+++ 042-20191009.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func trap(height []int) int {
+	size := len(height)
+	if size == 0 {
+		return 0
+	}
+
+	result := 0
+	leftMaxArr := make([]int, size, size)
+	rightMaxArr := make([]int, size, size)
+
+	for l := 0; l < size; l++ {
+		leftMaxArr[l] = height[l]
+		if l > 0 && leftMaxArr[l] < leftMaxArr[l-1] {
+			leftMaxArr[l] = leftMaxArr[l-1]
+		}
+	}
+
+	for r := size - 1; r >= 0; r-- {
+		rightMaxArr[r] = height[r]
+		if r < size-1 && rightMaxArr[r] < rightMaxArr[r+1] {
+			rightMaxArr[r] = rightMaxArr[r+1]
+		}
+	}
+
+	for i := 1; i < size; i++ {
+		var currentHight int
+		if rightMaxArr[i] > leftMaxArr[i] {
+			currentHight = leftMaxArr[i]
+		} else {
+			currentHight = rightMaxArr[i]
+		}
+		// fmt.Println("i is ", i, "height is ", currentHight, height[i])
+		if currentHight < height[i] {
+			continue
+		}
+		result += (currentHight - height[i])
+	}
+
+	return result
+}
+
+func main() {
+	arr := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
+	fmt.Println("trap is ", trap(arr))
+}
