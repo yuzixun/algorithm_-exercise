d4f4075e6c50a8049dbf4afbb8eedef83b3c5c29
yuzixun <yzxun@qq.com>
2019-10-20 21:42:55 +0800

    122. Best Time to Buy and Sell Stock II

---
+++ 122-20191020.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func maxProfit(prices []int) int {
+	var result int
+	for i := 0; i < len(prices)-1; i++ {
+		delta := (prices[i+1] - prices[i])
+		if delta > 0 {
+			result += delta
+		}
+	}
+	return result
+}
+
+func main() {
+	// prices := []int{1, 2, 3, 4, 5}
+	prices := []int{}
+
+	fmt.Println(maxProfit(prices))
+}
